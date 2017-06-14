9f1d1880c9ef3b03192698aac5bb16e894905f58
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-06-14 14:25:37 +0530

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,20 @@
+package Basics;
+import java.util.Scanner;
+public class Pair {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Enter the no.of students and pair");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int p=s.nextInt();
+for(int i=0;i<2*n;i++){
+	int d=2*n-i;
+	if(p!=d)
+	{   for(int j=1;j<=n;j++)
+		System.out.println(j+":"+p+"Pair");
+	
+	}
+	}
+
+	}}
