9feae14f3c93c767dd8ed052295d906b7bc7b7cc
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-06-20 17:56:00 +0530

    Add files via upload

---
+++ StringToInt.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class StringToInt {
+
+	public static void main(String[] args) {
+		String s = "10";
+		Integer n = Integer.valueOf(s);
+		System.out.println(n);
+
+	}
+
+}
