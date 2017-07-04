35fa8107443432b8fa5f3e276b094f4b82b5ebfe
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-07-04 09:51:26 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+	int input=56;
+	if(input%2==0)
+	{
+
+System.out.print("The given number is Even number");
+	}
+	else 
+	{
+
+System.out.print("The given number is Odd number");
+	}
+	}
+
+}
