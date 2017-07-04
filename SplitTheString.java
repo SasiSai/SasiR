d2fee0e8763dc7579e301b33cfa93591c20da3f3
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-07-04 09:47:58 +0530

    Add files via upload

---
+++ SplitTheString.java
@@ -0,0 +1,35 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class SplitTheString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the String");
+String str=s.next();
+int l=str.length();
+char c[];
+c=str.toCharArray();
+int count=1;
+int k=0;
+int m=0;
+for(int i=0;i<l-1;i++)
+{for(int j=i+1;j<l;j++)
+{
+	 if(c[i]==c[j])
+	 {
+		 count++;
+	 }
+	 
+} if(count>k)
+{
+	m=i;
+	k=count;
+	}count=1;
+}System.out.println(c[m]+" occurs "+k+" times");
+
+
+
+}}
