6b2c326aee443ea1192268d6504276f85f704929
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-07-19 10:09:11 +0530

    Add files via upload

---
+++ Tower.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Tower {
+
+	public static void main(String[] args) {
+System.out.println("Enter the word");
+Scanner s=new Scanner(System.in);
+String str=s.next();
+if(str.length()>1)
+{
+	System.out.println(str.charAt(str.length()-1)+str.substring(1,str.length()-1)+str.charAt(0));
+}else
+{
+	System.out.println(str);
+}
+	}
+
+}
