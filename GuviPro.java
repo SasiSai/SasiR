3085a7d31b64cc00f58da7e5cf4f67ad5e054845
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-07-19 10:38:53 +0530

    Add files via upload

---
+++ GuviPro.java
@@ -0,0 +1,15 @@
+package Basics;
+	import java.util.Scanner;
+
+	public class GuviPro {
+		public static void main(String[] args) {
+			System.out.println("Enter the rows and columns");
+			Scanner sc=new Scanner(System.in);
+			int l=sc.nextInt();
+			int w=sc.nextInt();
+			int cut=0;
+			if(l!=0 && w!=0)
+				cut=l*w-1;
+			System.out.println("Total cuttings is "+cut);
+		}}
+
