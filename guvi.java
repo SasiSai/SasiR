63b1f613424dc462c13acd41ec3d755e930abc47
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-07-31 14:47:46 +0530

    Update and rename guvi.java.txt to guvi.java

--- guvi.java
+++ guvi.java
@@ -3,15 +3,15 @@ public class Guvi {
     public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	String str=in.next();
-	String sub = null;int c,l=str.length();
+	String sub = null;int t,l=str.length();
         for(int i=1;i<l;i++){
-	   c=str.charAt(i)-str.charAt(i-1);
-	         if(c!=-1){
+	   t=str.charAt(i)-str.charAt(i-1);
+	         if(t!=-1){
 			 sub=str.substring(i);
 			 break;	}
 	if(sub.length()!=0)
           System.out.println("Lexographic Substring:"+sub);
 	
 	else
-	  System.out.println("no lexographic String can be formed");
+	  System.out.println( String can't be formed");
     }}}
