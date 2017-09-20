3a3f1425c0fcbd6d6f2b3a3af1af732c79264b6b
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-09-20 20:59:49 +0530

    Add files via upload

---
+++ unique.java
@@ -0,0 +1,27 @@
+package Guvi;
+import java.util.*;
+public class unique {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int a[] = new int[n];
+    int count[] = new int[n];
+    for(int i=0;i<n;i++)
+    {
+    	a[i] = sc.nextInt();
+    	count[a[i]]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+    	if(count[a[i]]==1)
+    	{
+    		System.out.println(a[i]);
+    	}
+    }
+    
+    sc.close();
+    
+	}
+
+}
