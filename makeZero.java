da028ac8a2924b03e0520b69fd2578ede4c5b089
SasiSai <sasikumar.rece@ksrce.ac.in>
2017-09-20 20:51:10 +0530

    Update makeZero.java

--- makeZero.java
+++ makeZero.java
@@ -1,5 +1,22 @@
 package Guvi;
-
+import java.util.*;
 public class makeZero {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+    int a[] = new int[n];
+    for(int i=0;i<n;i++)
+    {
+    	a[i] = sc.nextInt();
+    }
+    for(int i=0;i<n-1;i++){
+   for(int j=i+1;j<n;j++)
+   {
+	   if(a[i]+a[j] ==0){
+	   System.out.println(a[i]+","+a[j]);}
+   }}
+    sc.close();
+    	}
 
 }
